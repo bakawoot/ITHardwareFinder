f964f428b4e6fbbf4b09bc38712c2f451e567666
bakawoot <agodaniel1@gmail.com>
2021-11-24 10:18:25 +0100

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <div id="sidebar" class="sidebar">
-    <span id="demo"></span>
+    <span id="filterSliderCoreMinValue"></span>
     <input type="range" min="<?php echo(min(array_filter(array_column($rows, '# of CPU Cores'))));?>" max="<?php echo(max(array_column($rows, '# of CPU Cores')));?>" value="1" class="slider" onchange="filterCore(this)" id="filterSliderCoreMin">
     <input type="range" min="<?php echo(min(array_filter(array_column($rows, '# of CPU Cores'))));?>" max="<?php echo(max(array_column($rows, '# of CPU Cores')));?>" value="<?php echo max(array_column($rows, '# of CPU Cores'));?>" class="slider" onchange="filterCore(this)" id="filterSliderCoreMax">
-    <span id="demo2"></span>
+    <span id="filterSliderCoreMaxValue"></span>
 </div>
