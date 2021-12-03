7a2e2782de882cfcbc215bf1d01e664573dd4c6d
bakawoot <agodaniel1@gmail.com>
2021-12-03 19:29:33 +0100

    Update sidebar.js

--- include/sidebar.js
+++ include/sidebar.js
@@ -1,62 +1,163 @@
 document.getElementById("Filters").style.visibility = "visible";
+document.getElementById("content").style.visibility = "visible";
 
-const rows = document.querySelectorAll("tbody tr");
+const productTableRows = document.querySelectorAll("tbody tr");
+const inputFields = document.getElementById('Filters').getElementsByTagName('input');
+const arrayLength = inputFields.length;
 //console.log(rows);
 
-//1 input (checkbox)
-function filterPackage(inputID)
-{
-  console.log(inputID);
-  content = inputID;
-  rows.forEach((row) => {
-    if (row.querySelector("#prodTableProductAttributePackage").textContent != content.trim())
-    {
-      row.style.display = 'none';
-    } else
-      {
-        row.style.display = 'table-row';
-      }
+var numOfVisibleRows;
 
- });
-}
-
-//2 input (text input)
-function filterCore()
+function filter(inputID)
 {
-  var filterCoreMinValue = Number(document.getElementById("filterCoreMin").value);
-  var filterCoreMaxValue = Number(document.getElementById("filterCoreMax").value);
-  console.log(filterCoreMinValue);
-  console.log(filterCoreMaxValue);
-  console.log("lefut");
-  rows.forEach((row) =>
+  console.log('Function is called');
+  inputID = inputID;
+  toggleCheckboxes(inputID);
+
+  //show every row
+  //needed to refresh the filter
+  productTableRows.forEach((row) =>
   {
-    if ((Number(row.querySelector("#prodTableProductAttributeNumOfCores").textContent) >= filterCoreMinValue) && Number(row.querySelector("#prodTableProductAttributeNumOfCores").textContent) <= filterCoreMaxValue)
+    row.style.display = 'table-row';
+  });
+
+  for (var i = 0; i < arrayLength; i++)
+  {
+
+    //text input
+    if (inputFields[i].type == 'text')
     {
-      row.style.display = 'table-row';
-    }else {
-      row.style.display = 'none';
+      var filterCoreMinValue = Number(document.getElementById("filterCoreMin").value);
+      var filterCoreMaxValue = Number(document.getElementById("filterCoreMax").value);
+
+      productTableRows.forEach((row) =>
+      {
+        if (row.style.display == 'table-row'){
+          if ((Number(row.querySelector("#prodTableProductAttributeNumOfCores").textContent) >= filterCoreMinValue) && Number(row.querySelector("#prodTableProductAttributeNumOfCores").textContent) <= filterCoreMaxValue)
+          {
+            row.style.display = 'table-row';
+          } else
+          {
+            row.style.display = 'none';
+          }
+        }
+      });
+    }
+
+    //checkbox input
+    numOfItems = 0;
+    if (inputFields[i].type == 'checkbox')
+    {
+      if (String(inputFields[i].checked) == 'true')
+      {
+
+        //console.log('Checkbox is checked');
+        productTableRows.forEach((row) =>
+        {
+          if (row.style.display == 'table-row')
+          {
+            if (row.querySelector("#prodTableProductAttributePackage").textContent == (inputFields[i].parentElement.firstChild.nextElementSibling.textContent).trim())
+            {
+              row.style.display = 'table-row';
+              numOfItems++;
+            }
+          }
+        });
+      }else
+      {
+
+        //console.log('Checkbox is not checked');
+        productTableRows.forEach((row) =>
+        {
+          if (String(row.querySelector("#prodTableProductAttributePackage").textContent) == inputFields[i].parentElement.firstChild.nextElementSibling.textContent.trim())
+          {
+            row.style.display = 'none';
+            numOfItems++;
+          }
+        });
+      }
+      if (inputFields[i].id != 'togglePackageCheckboxes')
+      {
+        inputFields[i].parentElement.lastChild.textContent = " (" + numOfItems  + ")";
+      }
+    }
+
+  }
+  numOfVisibleRows = 0;
+
+  productTableRows.forEach((row) =>
+  {
+    if (row.style.display == 'table-row')
+    {
+      numOfVisibleRows++;
     }
   });
-}
+  document.getElementById("numOfItems").textContent = String(numOfVisibleRows);
 
-
-function filter()
-{
-  inputFields = document.getElementById('Filters').getElementsByTagName('input');
-  var arrayLength = inputFields.length;
-  for (var i = 0; i < arrayLength; i++) {
-    //console.log(inputFields)
-      if (inputFields[i].type == 'text')
-      {
-        console.log(inputFields[i].value);
-      }
-      if (inputFields[i].type == 'checkbox')
-      {
-        //console.log(inputFields[i].parentElement.textContent);
-        console.log(inputFields[i].checked)
-      }
-      //Do something
-  }
+  console.log('---');
 }
 
 filter();
+
+function toggleCheckboxes(inputID)
+{
+  numOfChecked = 0;
+  numOfFilterPackageInputs = 0;
+  for (var i = 0; i < arrayLength; i++)
+  {
+    if (inputFields[i].id == 'filterPackage' && inputFields[i].checked == '')
+    {
+      numOfChecked++;
+    }
+    if (inputFields[i].id == 'filterPackage')
+    {
+      numOfFilterPackageInputs++;
+    }
+  }
+  if (numOfChecked == numOfFilterPackageInputs)
+  {
+    for (var i = 0; i < arrayLength; i++)
+    {
+      if (inputFields[i].id == 'filterPackageAll')
+      {
+        inputFields[i].checked = 'true';
+        inputFields[i].disabled = 'true';
+      }
+    }
+  }
+
+  switch (inputID.id){
+    case 'filterPackageAll':
+    {
+      if (String(inputID.checked) == 'true')
+      {
+        inputID.disabled = 'true';
+        for (var i = 0; i < arrayLength; i++)
+        {
+          if (inputFields[i].id == 'filterPackage')
+          {
+            inputFields[i].checked = '';
+          }
+        }
+      } else
+      {
+        inputID.disabled = '';
+      }
+      break;
+    }case 'filterPackage':
+    {
+      if (String(inputID.checked) == 'true')
+      {
+        for (var i = 0; i < arrayLength; i++)
+        {
+          if (inputFields[i].id == 'filterPackageAll')
+          {
+            inputFields[i].checked = '';
+            inputFields[i].disabled = '';
+          }
+        }
+      }
+      break;
+    }
+  }
+}
